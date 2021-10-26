7547433833ad3e9bf789679b4404359b39de1874
Patrick <pmcguire96@gmail.com>
2021-10-25 22:50:55 -0500

    Data Structure Stuff
    Great overview plus some tuple material.

---
+++ McGuire XRTrading Tuple Code Challenge.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <tuple>
+using namespace std
+
+double tupleSum = 0;
+string castObj = "";
+int ignored = 0;
+
+int main(){
+	int numObjects = tuple_size(T)
+	using Tuple = std::tuple<int32_t,bool,std::string,float, const char*>;
+	Tuple t{10, true, "Modern", 2.3f, "C++";}
+	for (int i = 0; i++; i < numObjects) {
+		auto Tuple
+		}
+	
+	if (typeid(aint) == typeid(int)) {
+		tupleSum += aint; }
+	else if ((typeid(aint) == typeid(std::string))) {
+		castObj += aint; }
+	else {ignored++;}
+	
+	std::cout << tupleSum << ", " << castObj << ", " ignored
+	
+}
